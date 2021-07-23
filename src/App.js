69b91b5ec64a82cd8f284b11a09b57d51011b54b
Gerald Arzola <geraldarzola@gmail.com>
2021-07-23 13:31:55 -0400

    create LandingPage component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
+import LandingPage from './containers/LandingPage';
 
 function App() {
   return (
     <>
       <Switch>
         <Route exact path='/'>
-          <div className="App">
-            Hello
-          </div>
+          <LandingPage/>
         </Route>
 
-        <Route>
+        <Route exact path='/trial'>
           <div className="App">
-            
+            Test
           </div>
         </Route>
 
