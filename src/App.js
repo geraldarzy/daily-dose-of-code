86dfba6af15b52c9f062ca4635814e55e63209c1
Gerald Arzola <geraldarzola@gmail.com>
2021-07-23 13:51:52 -0400

    gave navbar props

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           <LandingPage/>
         </Route>
 
-        <Route exact path='/trial'>
+        <Route exact path='/home'>
           <HomePage/>
         </Route>
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
-const NavBar = () => {
+const NavBar = ({name='Guest', dayCount='1'}) => {
     return (
         <>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Daily Dose Of Code</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="home">Home</Nav.Link>
                             <Nav.Link href="#link">Link</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -19,6 +19,12 @@ const NavBar = () => {
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
+                    </Navbar.Collapse>
+
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            Signed in as: {name} | Day: {dayCount}
+                        </Navbar.Text>
                     </Navbar.Collapse>
                 </Container>
         </Navbar>
