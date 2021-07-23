2ffddc19a3a41adc51df86dacaddc70e766848e9
dshehbaj <shehbaj.dhillon@gmail.com>
2021-07-23 14:28:00 -0700

    Added amplify code.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, useHistory} from 'react-router-dom'
 import LandingPage from './containers/LandingPage';
 import HomePage from './containers/HomePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Auth } from 'aws-amplify';
+
+
 function App() {
+
+  const history = useHistory();
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, userIsAuthenticating] = useState(true);
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    history.push('/login');
+  };
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (e) {
+      if(e !== "No current user") {
+        alert(e);
+      }
+    }
+    userIsAuthenticating(false);
+  };
+
+
   return (
     <>
       <Switch>
---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_VNnEbKRG6',
+    APP_CLIENT_ID: '1lodeb9jrdekn7l7ujvtl8mopu',
+    IDENTITY_POOL_ID: 'us-east-1:fa54cc73-2b52-48ba-9458-f58452a43c5a'
+  }
+};
